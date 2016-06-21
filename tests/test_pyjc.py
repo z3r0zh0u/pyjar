2d87bf7e23fc20ae2c97cc7b0e4aa2389109ac62
z3r0zh0u <littlefater2012@gmail.com>
2016-06-21 09:37:22 +0800

    Reschedule the project structure

--- tests/test_pyjc.py
+++ tests/test_pyjc.py
@@ -7,22 +7,22 @@ import sys
 
 
 sys.path.append(os.path.abspath(".."))
-import pyjar.pyjar as pyjar
+import module.pyjc as pyjc
 
 
 if __name__ == '__main__':
 
     try:
-        java_class = pyjar.JavaClass('non_exist.class', debug = True)
+        java_class = pyjc.JavaClass('non_exist.class', debug = True)
     except Exception as e:
         print e
     print '-' * 40
 
     try:
-        java_class = pyjar.JavaClass('non_class.class', debug = True)
+        java_class = pyjc.JavaClass('non_class.class', debug = True)
     except Exception as e:
         print e
     print '-' * 40
     
-    java_class = pyjar.JavaClass('HelloWorld.class', debug = True)
+    java_class = pyjc.JavaClass('HelloWorld.class', debug = True)
 
